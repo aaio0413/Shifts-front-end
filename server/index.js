e2d6c096fb8bd2529faa23b4e90ed2b629859d8e
AAIO1992 <takahiro04kawaguchi13@gmail.com>
2018-12-22 17:00:43 -0800

    THis is a mess now...I have to fix view engine and render pug files.

--- server/index.js
+++ server/index.js
@@ -7,32 +7,32 @@ const app = express();
 // set view engine
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "../views"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 // set up routes
-// app.use("/auth", authRoutes);
+app.use("/auth", authRoutes);
 
 // create home route
 app.get("/", (req, res) => {
-  res.render("index", { title: "this is test", message: "Hello World" });
+  res.render("index");
 });
 
 app.get("/login", (req, res) => {
   res.render("login");
 });
 
-app.get("*", (req, res) => {
-  res.render("static" + req.url, function(err, html) {
-    if (!err) {
-      return res.send(html);
-    }
-    // Not super elegant the `indexOf` but useful
-    if (err.message.indexOf("Failed to lookup view") !== -1) {
-      return res.render("root/error");
-    }
-    throw err;
-  });
-});
+// app.get("*", (req, res) => {
+//   res.render("static" + req.url, function(err, html) {
+//     if (!err) {
+//       return res.send(html);
+//     }
+//     // Not super elegant the `indexOf` but useful
+//     if (err.message.indexOf("Failed to lookup view") !== -1) {
+//       return res.render("root/error");
+//     }
+//     throw err;
+//   });
+// });
 
 app.listen(3000, () => {
   console.log("app now listening for requests on port 3000");
